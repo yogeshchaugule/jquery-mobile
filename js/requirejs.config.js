4160d34fe5f3814c7988a625d132c716df657f36
Ghislain Seguin <ghislain@gmail.com>
2013-12-31 17:54:22 -0700

    Build: Added new paths

--- js/requirejs.config.js
+++ js/requirejs.config.js
@@ -1,9 +1,16 @@
 requirejs.config({
 	"paths": {
 		// requireJS plugins
+		"css": "../external/requirejs/plugins/css",
+		"json": "../external/requirejs/plugins/json",
+		"normalize": "../external/requirejs/plugins/normalize",
+		"step": "../external/requirejs/plugins/step",
 		"text": "../external/requirejs/plugins/text",
-		"json": "../external/requirejs/plugins/json"
+
+		"styles": "../css",
+		"tests": "../tests"
 	},
+
 	"shim": {
 		"jquery.hashchange": [ "jquery" ],
 		"jquery.ui.widget": [ "jquery" ],
